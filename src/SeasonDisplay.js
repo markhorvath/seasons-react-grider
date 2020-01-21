86b51513a20d1767669aa54ff1c2acfd1e891137
markhorvath <markhorvath7@gmail.com>
2020-01-21 17:08:08 -0500

    init commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+    return (
+        <div className="SeasonDisplay">Season Display</div>
+    );
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    //automatically called with props?  ALWAYS have to call super(props);
+    constructor(props) {
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.state = { lat: null, lng: null};
+    }
+
+    //render always must be defined in class components
+    render() {
+        //check MDN docs for more
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return <div>Latitude: </div>;
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
