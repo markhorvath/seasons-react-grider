bb00d10ebb94538b7dcd5438548c2756998b5b15
markhorvath <markhorvath7@gmail.com>
2020-01-21 17:57:01 -0500

    added setstate for geolocation data, simple logic for rendering

--- src/index.js
+++ src/index.js
@@ -6,17 +6,34 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null, lng: null};
+        this.state = { lat: null, lng: null, errorMessage: ''};
+
+
+        //check MDN docs for more
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude, lng: position.coords.longitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
     }
+
+
 
     //render always must be defined in class components
     render() {
-        //check MDN docs for more
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        return <div>Latitude: </div>;
+        let divStyle = {color: 'blue', fontSize: 48, marginTop: 30};
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if(!this.state.errorMessage && this.state.lat && this.state.lng) {
+            return <div style={divStyle}>Latitude: {this.state.lat}, Longitude: {this.state.lng}</div>
+        }
+
+        return <div style={divStyle}>Loading</div>;
     }
 }
 
