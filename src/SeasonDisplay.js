f4ee658f88f2e73ea7ef97799fc3c265d50d9af4
markhorvath <markhorvath7@gmail.com>
2020-01-22 20:27:10 -0500

    added componentdidmount, refactored constructor, passed state as props to SeasonDisplay compoenent, updated SeasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    //if statement and ternarys to determine season based on month and lattitude
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    // this logic could also be put directly in the object (where text is)
+    const text = season === 'winter' ? "Burr, it's chilly" : "Beach time";
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
+
     return (
-        <div className="SeasonDisplay">Season Display</div>
+        <div>
+    {/*the back-ticks are template strings from ES2015, takes the value of whatever icon is and passes it as a string*/}
+        <i className={`${icon} icon`} />
+        <h1>{text}</h1>
+        <i className={`${icon} icon`} />
+        <br />Latitude: {props.lat}, Longitude: {props.lng}</div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    //automatically called with props?  ALWAYS have to call super(props);
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null, lng: null, errorMessage: ''};
+    //this is completely equivalent to the constructor with super(props) and this.state
+    state = { lat: null, errorMessage: '' };
 
-
+    componentDidMount() {
         //check MDN docs for more
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude, lng: position.coords.longitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude, lng: position.coords.longitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
+
+    //any time component updates, render() will be called first before componentdidupdate()
+//     componentDidUpdate() {
+// console.log('ddi update');
+//     }
 
 
 
     //render always must be defined in class components
     render() {
-        let divStyle = {color: 'blue', fontSize: 48, marginTop: 30};
+        let divStyle = {color: 'blue', fontSize: 48, marginTop: 60};
         if(this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if(!this.state.errorMessage && this.state.lat && this.state.lng) {
-            return <div style={divStyle}>Latitude: {this.state.lat}, Longitude: {this.state.lng}</div>
+            return <SeasonDisplay lat={this.state.lat} lng={this.state.lng} />
         }
 
         return <div style={divStyle}>Loading</div>;
